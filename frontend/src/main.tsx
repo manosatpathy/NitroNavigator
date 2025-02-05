9d7f4736815571760b4d3aa29382013ff7c45303
manosatpathy <manosatpathy@gmail.com>
2025-02-05 23:30:01 +0530

    feat: set up project layout with initial structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import MainLayout from "./layouts/MainLayout.tsx";
-import Header from "./components/Header.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
+import Homepage from "./pages/Homepage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+    ],
   },
 ]);
 

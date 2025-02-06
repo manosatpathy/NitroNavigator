ae63b683946a6c2552c8e1e9033e4e796d76de52
manosatpathy <manosatpathy@gmail.com>
2025-02-06 20:45:11 +0530

    feat: add user registration route with validation and JWT authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Db"));
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", (req: Request, res: Response) => {
-  return res.json({ message: "Hello from express app" });
-});
+app.use("/api/users", userRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });

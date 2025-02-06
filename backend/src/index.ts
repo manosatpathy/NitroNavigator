a0a53817992433590527815c95c4dcd128d5d3b1
manosatpathy <manosatpathy@gmail.com>
2025-02-06 22:47:51 +0530

    feat: implement login route with validation and jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");

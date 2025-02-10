400270cec064486b97e77219fba8712f7126b80f
manosatpathy <manosatpathy@gmail.com>
2025-02-10 23:57:47 +0530

    style: update header design with sticky positioning and dark theme

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="bg-slate-300 py-6 lg:px-8 px-2 font-poppins">
+    <header className="sticky bg-[#1c1c1c] top-0 py-6 font-poppins bg-background/95">
       <div className="container mx-auto flex justify-between items-center">
-        <span className="text-4xl text-slate-800 font-poppins font-extrabold tracking-tighter">
+        <span className="text-4xl text-[#F8F9FA] font-poppins font-extrabold tracking-tighter">
           <Link to="/">Havenly</Link>
         </span>
         <span className="flex space-x-4">
           <Link
             to="/sign-in"
-            className="flex shadow-lg bg-slate-700 text-white items-center px-4 py-2 font-semibold rounded-md hover:bg-slate-200 hover:text-slate-900 transition duration-300"
+            className="flex shadow-lg bg-white text-blue-900 items-center px-4 py-2 font-semibold rounded-md hover:bg-blue-400 hover:text-white transition duration-300"
           >
             Sign In
           </Link>
         </span>
       </div>
-    </div>
+    </header>
   );
 };
 

b4a97237b93b3ff03f0ada7dbb76f02c6901cf8c
manosatpathy <manosatpathy@gmail.com>
2025-02-10 23:44:35 +0530

    feat: configure CORS to allow frontend access

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,12 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);

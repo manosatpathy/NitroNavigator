32db77caad5ae1a2df11bd1f643e878ff1937c97
manosatpathy <manosatpathy@gmail.com>
2025-02-14 23:28:24 +0530

    feat: add signOut function to API client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -43,3 +43,13 @@ export const validateToken = async () => {
   }
   return response.json();
 };
+
+export const signOut = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/signout`, {
+    credentials: "include",
+    method: "POST",
+  });
+  if (!response.ok) {
+    throw new Error("Error during sign out");
+  }
+};

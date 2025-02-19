fceeb0f7ecd9a2317c3afccff4087001af03f7dd
manosatpathy <manosatpathy@gmail.com>
2025-02-19 11:19:35 +0530

    refactor: remove unnecessary imports and static file serving

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -19,8 +18,6 @@ app.use(
     credentials: true,
   })
 );
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);

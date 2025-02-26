aa972b5f7d11eb5a674fbccbfe8071a338a01733
manosatpathy <manosatpathy@gmail.com>
2025-02-26 22:30:58 +0530

    refactor: move routing from main.tsx to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,44 @@
-const App = () => {
-  return <></>;
-};
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { useAppContext } from "./contexts/AppContext.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
+import Homepage from "./pages/Homepage.tsx";
+import Register from "./pages/Register.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import AddHotel from "./pages/AddHotel.tsx";
+
+function App() {
+  const { isLoggedIn } = useAppContext();
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/sign-in",
+          element: <SignIn />,
+        },
+        {
+          path: "/add-hotel",
+          element: isLoggedIn ? <AddHotel /> : <Navigate to="/sign-in" />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,48 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout.tsx";
-import Homepage from "./pages/Homepage.tsx";
-import Register from "./pages/Register.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppContextProvider } from "./contexts/AppContext.tsx";
-import SignIn from "./pages/SignIn.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Homepage />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/sign-in",
-        element: <SignIn />,
-      },
-    ],
-  },
-]);
+import App from "./App.tsx";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-  },
+  defaultOptions: { queries: { retry: 0 } },
 });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
-        <RouterProvider router={router} />
+        <App />
       </AppContextProvider>
     </QueryClientProvider>
   </StrictMode>

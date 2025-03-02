3438a8f8785ce4d697610be9ade50620c62e6039
manosatpathy <manosatpathy@gmail.com>
2025-03-02 23:57:37 +0530

    feat: integrate hotel saving functionality with mutation

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,5 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import ManageHotelForm from "./../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
+
 const AddHotel = () => {
-  return <ManageHotelForm />;
+  const { showToast } = useAppContext();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.addMyHotel,
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel!", type: "ERROR" });
+    },
+  });
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} isLoading={isPending} />;
 };
 
 export default AddHotel;

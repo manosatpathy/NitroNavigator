a6d4c03f4e4c2e14c2652b2bad21b8eee08e4da1
manosatpathy <manosatpathy@gmail.com>
2025-03-05 23:48:52 +0530

    fix: add missing key prop and improve error styling

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -14,8 +14,9 @@ const TypeSection = () => {
     <div>
       <h2 className="text-2xl font-bold mb-3 ">Type</h2>
       <div className="grid grid-cols-5 gap-2">
-        {hotelTypes.map((type) => (
+        {hotelTypes.map((type, index) => (
           <label
+            key={index}
             className={
               typeWatch === type
                 ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
@@ -32,7 +33,11 @@ const TypeSection = () => {
           </label>
         ))}
       </div>
-      {errors.type && <span>{errors.type.message}</span>}
+      {errors.type && (
+        <span className="text-red-500 text-sm font-bold">
+          {errors.type.message}
+        </span>
+      )}
     </div>
   );
 };

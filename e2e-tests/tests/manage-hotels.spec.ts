6f9fc28269d7bcaabd078fde7daa345966e211f4
manosatpathy <manosatpathy@gmail.com>
2025-03-07 23:27:16 +0530

    test: Add test for displaying hotels on my-hotels page

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -45,3 +45,19 @@ test("should allow user to add a hotel", async ({ page }) => {
   await page.getByRole("button", { name: "Save" }).click();
   await expect(page.getByText("Hotel Saved!")).toBeVisible();
 });
+
+test("should display hotels", async ({ page }) => {
+  await page.goto(`${UI_URL}/my-hotels`);
+  await expect(page.getByText("Ashraya Hotel")).toBeVisible();
+  await expect(
+    page.getByText("This is a hotel with gym facilities.")
+  ).toBeVisible();
+  await expect(page.getByText("Puri, india")).toBeVisible();
+  await expect(page.getByText("Budget")).toBeVisible();
+  await expect(page.getByText("₹1000 per night")).toBeVisible();
+  await expect(page.getByText("2 adults, 4 children")).toBeVisible();
+  await expect(page.getByText("3 Star Rating")).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});

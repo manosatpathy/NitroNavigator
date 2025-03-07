186b5d89188bbe402ae225ba5c31148d2a8e9daa
manosatpathy <manosatpathy@gmail.com>
2025-03-07 23:23:06 +0530

    feat: add EditHotel page

---
+++ frontend/src/pages/EditHotel.tsx
@@ -0,0 +1,40 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+
+const EditHotel = () => {
+  const { hotelId } = useParams();
+  const { showToast } = useAppContext();
+
+  const { data: hotelData } = useQuery({
+    queryKey: ["fetchMyHotelById", hotelId],
+    queryFn: () => apiClient.fetchMyHotelById(hotelId || ""),
+    enabled: !!hotelId,
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.updateMyHotelById,
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (
+    <ManageHotelForm
+      hotel={hotelData}
+      onSave={handleSave}
+      isLoading={isPending}
+    />
+  );
+};
+
+export default EditHotel;

3cffabff0108427c67bebf747fa90942ca2a7158
manosatpathy <manosatpathy@gmail.com>
2025-03-21 23:59:02 +0530

    test: add test for hotel search functionality

---
+++ e2e-tests/tests/search-hotel.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("m@m.com");
+  await page.locator("[name=password]").fill("Password@1403");
+
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page.getByText("Sign in Successful")).toBeVisible();
+});
+
+test("should show hotel search results", async ({ page }) => {
+  await page.goto(UI_URL);
+  await page.getByPlaceholder("Where are you going?").fill("Nayagarh");
+  await page.getByRole("button", { name: "Search" }).click();
+  await expect(page.getByText("Hotels found in nayagarh")).toBeVisible();
+  await expect(page.getByText("nayagarh hotel")).toBeVisible();
+});

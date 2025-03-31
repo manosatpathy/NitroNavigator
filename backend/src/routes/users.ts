c0762c3fcb272bd70e272a210edb24b4b6029282
manosatpathy <manosatpathy@gmail.com>
2025-03-31 23:29:04 +0530

    feat: Add endpoint to fetch authenticated user details

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -3,8 +3,24 @@ import User from "../models/user";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { check, validationResult } from "express-validator";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
+
+router.get("/me", verifyToken, async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(400).json({ message: "user not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
 router.post(
   "/register",
@@ -37,7 +53,7 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "none",
+        // sameSite: "none",
         maxAge: 86400000,
       });
       return res.status(200).send({ message: "User registered OK" });

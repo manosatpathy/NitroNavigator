370b07b654a31dbf65f1a22046f01bc7c37b69ab
manosatpathy <manosatpathy@gmail.com>
2025-04-09 23:45:39 +0530

    feat: add createRoomBooking API client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -6,6 +6,7 @@ import {
   PaymentIntentResponse,
   UserType,
 } from "../../backend/src/types";
+import { BookingFormData } from "./forms/BookingForm/BookingForm";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -190,4 +191,21 @@ export const createPaymentIntent = async (
     throw new Error("Error fetching payment intent");
   }
   return response.json();
+};
+
+export const createRoomBooking = async (formData: BookingFormData) => {
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Error booking room");
+  }
 };

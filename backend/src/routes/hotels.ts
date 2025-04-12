8b7f72e1b6d80d49ec89bd886bcda706729684c0
manosatpathy <manosatpathy@gmail.com>
2025-04-12 23:51:10 +0530

    feat: add route to get recently added hotels

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -52,6 +52,16 @@ router.get("/search", async (req: Request, res: Response) => {
   }
 });
 
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find().sort("-lastUpdated").limit(3);
+    res.json(hotels);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 router.get(
   "/:id",
   [param("id").notEmpty().withMessage("valid hotel ID is required.")],
@@ -156,7 +166,6 @@ router.post(
         return res.status(400).json({ message: "hotel not found" });
       }
 
-      await hotel.save();
       res.status(200).send();
     } catch (error) {
       console.log(error);

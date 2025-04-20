b01eee85eefbc19edbe8d1d0d0688cd063514e5b
manosatpathy <manosatpathy@gmail.com>
2025-04-20 23:46:43 +0530

    feat(ui): revamp SignIn page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -36,57 +36,81 @@ const SignIn = () => {
   const onSubmit = handleSubmit((data) => {
     mutation.mutate(data);
   });
-  return (
-    <div className="flex items-center justify-center container my-auto bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat">
-      <form
-        action=""
-        className="w-full max-w-lg p-8 rounded-lg bg-white/20 shadow-lg backdrop-blur-sm"
-        onSubmit={onSubmit}
-      >
-        <h1 className="font-bold text-2xl text-white mb-3">Sign In</h1>
 
-        <label className="text-white text-sm font-bold mt-4">
-          Email
-          <input
-            type="email"
-            className="border rounded w-full mt-1 p-2 text-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            {...register("email", { required: "This field is required." })}
+  return (
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="flex flex-col md:flex-row w-full max-w-4xl bg-white/30 backdrop-blur-sm rounded-lg overflow-hidden shadow-lg">
+        <div className="w-full md:w-1/2 relative overflow-hidden rounded-lg hidden md:block">
+          <img
+            src="/bg2.jpg"
+            alt="Background"
+            className="h-full w-full object-cover"
           />
-          {errors.email && (
-            <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
-          )}
-        </label>
-        <label className="text-white text-sm font-bold mt-4">
-          Password
-          <input
-            type="password"
-            className="border rounded w-full mt-1 p-2 text-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            {...register("password", {
-              required: "This field is required.",
-              minLength: {
-                value: 6,
-                message: "Password must be at least 6 characters.",
-              },
-            })}
-          />
-          {errors.password && (
-            <p className="text-red-500 text-sm mt-1">
-              {errors.password.message}
-            </p>
-          )}
-        </label>
-        <span className="flex items-end justify-between">
-          <span className="flex text-sm text-white flex-col">
-            Not Registered?{" "}
-            <Link to="/register" className="underline">
-              Create an account here
-            </Link>
-          </span>
-          <button className="bg-blue-600 text-white py-2 px-4 mt-6 font-bold rounded-lg hover:bg-blue-500 text-lg transition">
-            Sign In
+          <div className="absolute inset-0 bg-green-500/70 z-10"></div>
+        </div>
+        <form
+          className="w-full md:w-1/2 p-8 rounded-lg bg-white/30 shadow-lg backdrop-blur-sm"
+          onSubmit={onSubmit}
+        >
+          <h2 className="text-3xl font-bold text-gray-800 mb-6">Sign In</h2>
+
+          <div className="min-h-[100px]">
+            <label className="text-gray-700 text-sm font-bold">
+              Email
+              <input
+                type="email"
+                className="border rounded-lg w-full mt-1 py-3 px-4 text-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                {...register("email", { required: "This field is required." })}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </p>
+              )}
+            </label>
+          </div>
+
+          <div className="min-h-[100px]">
+            <label className="text-gray-700 text-sm font-bold">
+              Password
+              <input
+                type="password"
+                className="border rounded-lg w-full mt-1 py-3 px-4 text-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                {...register("password", {
+                  required: "This field is required.",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters.",
+                  },
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            disabled={mutation.isPending}
+            className="w-full bg-blue-600 text-white py-2 px-4 mt-6 font-bold rounded-lg hover:bg-blue-500 text-lg transition disabled:bg-gray-500"
+          >
+            {mutation.isPending ? "Signing In..." : "Sign In"}
           </button>
-        </span>
-      </form>
+
+          <div className="mt-6 text-sm text-center text-black flex justify-center gap-2">
+            Don't have an account?
+            <Link
+              to="/register"
+              className="text-blue-400 hover:underline transition duration-200 tracking-tighter"
+            >
+              Register here
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
